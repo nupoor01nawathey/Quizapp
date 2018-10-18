10bddb9fb3463144be0e05867ba1328ebdc5fa4a
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-10-18 17:53:44 +0530

    mlab connection string
    mlab connection string

--- config/secret-keys.js
+++ config/secret-keys.js
@@ -3,5 +3,9 @@ module.exports = {
     google: {
         clientID: 'YOUR_CLIENTID',
         clientSecret: 'YOUR_SECRET',
+    },
+    mongodb: {
+        dbURI: 'mongodb://YOUR_MLAB_DB_USERNAME:DB_PASSWORD@ds135413.mlab.com:35413/myproj'
+       
     }
 }
