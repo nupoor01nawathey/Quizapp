24ba39234ef9282e55f7e7d68233e08156c3f6db
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-10-20 16:20:35 +0530

    seeder for quiz category data
    seeder for quiz category data

---
+++ quizCategorySeeder.js
@@ -0,0 +1,27 @@
+quizCategoryInfo = [
+    {
+        title: 'DevOps',
+        description: 'DevOps is a software development methodology that combines software development (Dev) with information technology operations (Ops). The goal of DevOps is to shorten the systems development life cycle while also delivering features, fixes, and updates frequently in close alignment with business objectives.',
+        image: 'https://www.techwell.com/sites/default/files/stories/images/cropped_teasers/Josiah%20Renaudin/2017/dev.png',
+    },
+    {
+        title: 'Modern Web Dev',
+        description: 'Most of the technology involved in the web, like the hypertext, like the Internet, multifont text objects, had all been designed already. I just had to put them together. — Tim Berners-Lee',
+        image: 'https://i.ytimg.com/vi/VhnhcW1FBLw/maxresdefault.jpg',
+    },
+    {
+        title: 'Databases',
+        description: 'A database is an organized collection of data, stored and accessed electronically. Database designers typically organize the data to model aspects of reality such as (for example) modeling the availability of rooms in hotels in a way that supports finding a hotel with vacancies.',
+        image: 'https://i.ytimg.com/vi/BxjpuidOW9g/maxresdefault.jpg',
+    },
+    {
+        title: 'Cloud Computing',
+        description: 'Cloud computing is shared pools of configurable computer system resources and higher-level services that can be rapidly provisioned with minimal management effort, often over the Internet.',
+        image: 'https://4zy7s42hws72i51dv3513vnm-wpengine.netdna-ssl.com/wp-content/uploads/2018/02/cloud-platform-comparative-listings-img.jpg',
+    },
+]
+
+
+
+
+module.exports = quizCategoryInfo ;
